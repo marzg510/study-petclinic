7d31ee51b688bc734b8e31597bb050dac1352bd5
masaru <masaru.gotou@gmail.com>
2026-03-20 03:44:33 +0000

    petclinic.tfをmain.tfに統合

--- spring-petclinic-aws-ecs/main.tf
+++ spring-petclinic-aws-ecs/main.tf
@@ -10,3 +10,69 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_service_discovery_http_namespace" "petclinic" {
+  name = "petclinic"
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "petclinic"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  service_connect_defaults {
+    namespace = aws_service_discovery_http_namespace.petclinic.arn
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "petclinic-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_ssm" {
+  name = "petclinic-task-ssm-policy"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
